461bdf287b1e393315bfa09181ad244be7e5f255
JungJ1na <wjdwlsk0211@gmail.com>
2022-05-24 16:23:46 +0900

    영화 정보의 수

---
+++ app/src/main/java/com/example/a11st_app/Movie.java
@@ -0,0 +1,18 @@
+package com.example.a11st_app;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rank01dAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String salesAnt;
+    String salesShare;
+    String salesInten;
+    String salesChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+}
